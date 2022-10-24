289582a5579dd152e2951846eebf1aed1b192332
DeadlyAlien <nicog99@live.com>
2022-10-24 04:13:46 -0500

    added environment variables for encryption key

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 //jshint esversion:6
+require("dotenv").config();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 const express = require("express");
 const encrypt = require("mongoose-encryption");
 
+
 const app = express();
+
+console.log(process.env.API_KEY);
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
-
-
-
-
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true});
 
@@ -23,10 +25,11 @@ const userSchema = new mongoose.Schema ({
     password: String
 });
 
-const secret = "Thisisourlittlesecret.";
+
 
 // this plugin for encryption must be added before creating a model using the mentioned schema
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+// use secret: secret* for a 'secret string'. * being a variable
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 
 const User = new mongoose.model("User", userSchema);
