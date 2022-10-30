380063c47f1a02d5257f07c06d9e38d42b772a29
DeadlyAlien <nicog99@live.com>
2022-10-30 14:42:27 -0500

    Added MD5 Hash to passwords

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 const express = require("express");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+
 
 
 const app = express();
@@ -26,10 +27,6 @@ const userSchema = new mongoose.Schema ({
 });
 
 
-
-// this plugin for encryption must be added before creating a model using the mentioned schema
-// use secret: secret* for a 'secret string'. * being a variable
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 
 const User = new mongoose.model("User", userSchema);
@@ -69,7 +66,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err) {
@@ -84,7 +81,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser) {
         if (foundUser) {
