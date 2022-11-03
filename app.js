b3b2cdc70fa66eab300b3a62298ec52836ce635c
DeadlyAlien <nicog99@live.com>
2022-11-03 16:35:24 -0500

    Added GoogleOAuth and added Bootstrap CSS file

--- app.js
+++ app.js
@@ -4,41 +4,91 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 const express = require("express");
-const bcrypt = require("bcrypt");
-const saltRounds = 10; 
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+
+
 
 
 
 const app = express();
 
-console.log(process.env.API_KEY);
-
+app.use(express.static("public"));
 app.set("view engine", "ejs");
-
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
     extended:true
 }));
 
+
+
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    // saveUninitialized: false, is useful for implementing logins and reducing server storage use.
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
 // mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true});
 mongoose.connect("mongodb://127.0.0.1/userDB", {useNewUrlParser:true});
+
+// mongoose.set("useCreateIndex", true);
 
 
 const userSchema = new mongoose.Schema ({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
+
+userSchema.plugin(passportLocalMongoose);
+// this uses the plugin to make googleOAuth work
+userSchema.plugin(findOrCreate);
 
 
 
 const User = new mongoose.model("User", userSchema);
 
 
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+   
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
 
 
 
-//test
+// This must be in the proper location, after declaration of the variable and session. 
+// This is for google auth
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
+
+
 
 
 
@@ -48,15 +98,19 @@ app.get("/", function(req, res) {
 
 
 
+app.get("/auth/google", passport.authenticate("google", { scope: ["profile"] }));
 
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect('/secrets');
+  });
 
 
 app.get("/login", function(req, res) {
     res.render("login");
 });
-
-
-
 
 
 app.get("/register", function(req, res) {
@@ -65,43 +119,70 @@ app.get("/register", function(req, res) {
 
 
 
+app.get("/secrets", function(req, res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    };
+})
+
+
+app.get("/logout", function(req, res) {
+    req.logout(function(err) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.redirect("/")
+        }
+    });
+});
+
+
+
+
 
 app.post("/register", function(req, res) {
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-    
-        newUser.save(function(err) {
-            if (!err) {
-                res.render("secrets");
-            } else {
-                console.log(err);
-            }
-        });
-    });
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets")
+            })
+        }
+    })
 
 });
 
 
-app.post("/login", function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
 
-    User.findOne({email: username}, function(err, foundUser) {
-        if (foundUser) {
-            bcrypt.compare(password, foundUser.password, function(err, result) {
-                if (result === true) {
-                    res.render("secrets");
-                };
-            });
-        } else {
+// Run passport.authenticate in the post request so that it checks to see if the login is correct before displaying /secrets
+// If this is done in the req.login, it can be bypassed by using websiteURL/secrets
+app.post("/login", passport.authenticate("local"), function(req, res) {
+    
+
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err) {
+        if (err) {
             console.log(err);
+        } else {
+            res.redirect("/secrets");
         };
     });
+
+
 });
+
+
+
+
 
 
 
